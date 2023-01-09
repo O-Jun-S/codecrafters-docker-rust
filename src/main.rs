3c980ccd89268658bb78bc1fa4a8717ba1d066de
O-Jun-S <jun.shima256@gmail.com>
2023-01-09 10:45:14 +0900

    pipe stdout/err to the parent process

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
+use std::process::Stdio;
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() {
-    println!("Logs from your program will appear here!");
-
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .unwrap();
 
     if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        println!("{}", std_out)
+        std::process::exit(0);
     } else {
         std::process::exit(1);
     }
